c64f5493ff845e4fdd129d096dc1eade24ce82e4
So Yeon <starwish024@gmail.com>
2021-06-12 19:41:15 +0900

    third commit

--- controllers/updateprivatepage.js
+++ controllers/updateprivatepage.js
@@ -1,12 +1,13 @@
 const Memo = require('../models/Memo')
 
 module.exports = async (req, res) => {
-
+    const memo = req.params.id
     Memo.findOneAndUpdate({ _id: req.params.id },
         req.body,  { returnOriginal: false }, function (err, update) {
             if (err) {
                console.log(err)
+               res.redirect('/memohome')
             }
         })
-    res.redirect('/memo/privatememo/req.params.id')
+    res.redirect('/memo/privatememo/'+memo)
 }
